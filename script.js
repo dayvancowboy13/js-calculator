599fd09c66ba124741775a41f9ad1bf699c338cb
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-05-21 23:04:18 -0700

    number and operator functionality almost working; hitting numbers show up on display; display limits to 10 characters; css/html is acceptable

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
 //console.log("testing 1,2, 1,2")
 
 // let firstNumber;
-// let operator;
+let operator = '';
 // let secondNumber;
+const MAX_DISPLAY_CHARS = 10;
+let display = document.querySelector(".display");
+ addListeners();
 
+ // on hitting EQUALS (=), then the calculator will call operate()
 
+function addListeners(){
+    let numberNodes = document.getElementsByClassName("number");
+    
+    // numbers
+    for (let i = 0; i < numberNodes.length; i++) {
+        numberNodes[i].addEventListener('click',() =>{
+            appendToDisplay(numberNodes[i].textContent);
+        })
+    }
+
+    // operators
+    let operatorNodes = document.getElementsByClassName("operator");
+    // console.log(operatorNodes)
+
+    for (let i = 0; i < operatorNodes.length; i++){
+        operatorNodes[i].addEventListener('click', ()=> {
+            operator = operatorNodes[i].textContent;
+            console.log(operator);
+        });
+    }
+}
+
+function clear(){}
+
+function appendToDisplay(value){
+    if(display.textContent == '0'){
+        display.textContent = value;
+    } else {
+        let currentDisplayText = display.textContent;
+        if (currentDisplayText.length < 10){
+            currentDisplayText += value;
+            display.textContent = currentDisplayText;
+        } else {
+            //do NOTHING
+        }
+    }
+}
 
 // OPERATE FUNCTION:
 function operate(operator, num1, num2){
